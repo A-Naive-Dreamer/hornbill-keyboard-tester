3c07e3d87daa2efd85dd3cca833f1d793d15b1ce
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-07-12 23:48:37 +0700

    1

--- js/functions-1.js
+++ js/functions-1.js
@@ -1,11 +1,24 @@
-$('.keyboard-buttons').mouseenter(function () {
+var records = []
+
+$(document).ready(function() {
+    $('#keyboard-input').focus()
+})
+
+$('#keyboard-input').keydown(function(event) {
+    console.log(event.which)
+})
+
+$('.keyboard-buttons').mouseenter(function() {
+    /*records.push(Date.now())
+    console.log(records)*/
+
     $(this).css({
         'background': 'rgb(145, 145, 145)',
         'transform': 'scale(0.9)'
     })
 })
 
-$('.keyboard-buttons').mouseout(function () {
+$('.keyboard-buttons').mouseleave(function () {
     $(this).css({
         'background': 'rgb(175, 175, 175)',
         'transform': 'scale(1.0)'
