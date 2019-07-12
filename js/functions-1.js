1867bf5b974ae5c947ed20bc6976bb67bb9274d0
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-07-13 00:26:25 +0700

    0.5

--- js/functions-1.js
+++ js/functions-1.js
@@ -1,11 +1,24 @@
-var records = []
+var records = [],
+    charCode = ''
 
 $(document).ready(function() {
     $('#keyboard-input').focus()
 })
 
 $('#keyboard-input').keydown(function(event) {
-    console.log(event.which)
+    charCode = '#' + event.key.toUpperCase()
+
+    $(charCode).css({
+        'background-image': 'url(../assets/images/icon-2.png)',
+        'transform': 'scale(0.9)'
+    })
+})
+
+$('#keyboard-input').keyup(function(event) {
+    $(charCode).css({
+        'background-image': 'url(../assets/images/icon-1.png)',
+        'transform': 'scale(1.0)'
+    })
 })
 
 $('.keyboard-buttons').mouseenter(function() {
