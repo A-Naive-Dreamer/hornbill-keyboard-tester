b3955493c7bd069e615bc5d27d45118f0f3e134d
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-10-31 02:59:47 +0700

    Web version 1.0

--- resources/app/js/functions-2.js
+++
@@ -1,9 +0,0 @@
-var keyboardType = document.getElementById('keyboard-type'),
-    testButton = document.getElementById('test-button')
-
-testButton.addEventListener('click', () => {
-    let type = keyboardType.value.toLowerCase().trim().replace(/\s/g, '-').replace(/[^a-zA-Z-]/g, ''),
-        url = '../keyboard-layouts/' + type + '.html'
-
-    location.assign(url)
-})
